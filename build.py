427cfab310e80e4ca43f26c962bd6f9d8a0bc5e2
SuffixAutomata <piggydan628318@protonmail.com>
2023-01-08 21:16:57 +0800

    t h e

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 import glob
 import shutil
 import os
+import os.path
 import distutils.dir_util
 
 shutil.rmtree("build", ignore_errors=True)
@@ -9,6 +10,7 @@ os.mkdir("build")
 distutils.dir_util.copy_tree("bees", "build")
 distutils.dir_util.copy_tree("comics", "build/comics")
 
-l = [i[6:] for i in glob.glob('build/**', recursive=True)]
+l = [i[6:]
+     for i in glob.glob('build/**/*', recursive=True) if os.path.isfile(i)]
 
 print(*l, sep='\n', file=open("build/index.html", "w"))
