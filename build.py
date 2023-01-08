1f406be6d5c6a898cf40d732401122480b9052c8
SuffixAutomata <piggydan628318@protonmail.com>
2023-01-08 21:07:33 +0800

    WILL THIS WORK? CLICK HERE TO FIND OUT

---
+++ build.py
@@ -0,0 +1,14 @@
+import glob
+import shutil
+import os
+import distutils.dir_util
+
+shutil.rmtree("build", ignore_errors=True)
+os.mkdir("build")
+
+distutils.dir_util.copy_tree("bees", "build")
+distutils.dir_util.copy_tree("comics", "build/comics")
+
+l = [i[6:] for i in glob.glob('build/**', recursive=True)]
+
+print(*l, sep='\n', file=open("build/index.html", "w"))
